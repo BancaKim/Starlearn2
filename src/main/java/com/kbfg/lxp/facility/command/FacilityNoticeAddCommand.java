4fe20083b476b422b43d77d7896b172ade4447e9
BancaKim <weberian92@gmail.com>
2024-07-01 13:46:35 +0900

    0701

--- src/main/java/com/kbfg/lxp/facility/command/FacilityNoticeAddCommand.java
+++ src/main/java/com/kbfg/lxp/facility/command/FacilityNoticeAddCommand.java
@@ -18,38 +18,9 @@ public class FacilityNoticeAddCommand implements Command {
 	
 	@Override
 	public void execute(Model model) {
-		
-<<<<<<< HEAD
+
 		ArrayList<FacilityNoticeBean> dtos = noticedao.list();
 		model.addAttribute("list",dtos);
-=======
-//		List noticelist=new ArrayList();
-//		Map<String, Object> map = model.asMap();
-//		HttpServletRequest request = (HttpServletRequest) map.get("request");
-//		
-//	  	int page=1;
-//		int limit=10;
-//		
-//		if(request.getParameter("page")!=null){
-//			page=Integer.parseInt(request.getParameter("page"));
-//		}
-//		
-//		int listcount=noticedao.getListCount(); 
-//		noticelist = noticedao.getBoardList(page,limit); 
-//		
-//		int maxpage=(int)((double)listcount/limit+0.95); 
-//   		int startpage = (((int) ((double)page / 10 + 0.9)) - 1) * 10 + 1;
-//   		int endpage = maxpage;
-//   		
-//   		if (endpage>startpage+10-1) endpage=startpage+10-1;
-//   		
-//   		model.addAttribute("page", page);		
-//   		model.addAttribute("maxpage", maxpage); 
-//   		model.addAttribute("startpage", startpage);
-//   		model.addAttribute("endpage", endpage);     
-//   		model.addAttribute("listcount",listcount); 
-//   		model.addAttribute("noticelist", noticelist);
->>>>>>> one0701
 
 	 }
  }
